9e436de35b5de4a7f1afb6dec4ef6a0a8d5c31f5
kasper <kasperfriangt@gmail.com>
2022-12-06 02:03:50 +0100

    added connection functionality

---
+++ ScSoMeAPI/ScSoMeAPI/Controllers/ConnectionsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScSoMeAPI.Models;
+
+namespace ScSoMeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConnectionsController
+    {
+        [HttpGet]
+        [Route("AllConnections")]
+        public List<Connection> GetAllConnections([FromQuery] string user)
+        {
+            using BachelordbContext ctx = new BachelordbContext();
+            List<Connection> connections = new List<Connection>();
+            try
+            {
+                foreach (var connection in ctx.Connections.Where(c => c.UsernameCon1 == user || c.UsernameCon2 == user))
+                {
+                    if (connection.UsernameCon2 == user)
+                    {
+                        connections.Add(new Connection
+                        {
+                            UsernameCon1 = user,
+                            UsernameCon2 = connection.UsernameCon1,
+                            CreatedDate = connection.CreatedDate
+                        });
+                    }
+                    else
+                    {
+                        connections.Add(connection);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                connections = new List<Connection>{ new Connection
+                {
+                   UsernameCon1 = "Error",
+                   UsernameCon2 = e.Message,
+                   CreatedDate = DateTime.Now
+
+                } };
+
+            }
+            return connections;
+        }
+        [HttpPost]
+        [Route("AddConnection")]
+        public Connection PostConnection([FromBody] Connection con)
+        {
+            using BachelordbContext ctx = new BachelordbContext();
+
+            try
+            {
+                
+                var result = ctx.Connections.Add(con);
+                ctx.SaveChanges();
+
+            }
+            catch (Exception e)
+            {
+                con = new Connection
+                {
+                    UsernameCon1 = "Error",
+                    UsernameCon2 = e.Message,
+                    CreatedDate = DateTime.Now
+
+                };
+
+            }
+            return con;
+        }
+    }
+}
